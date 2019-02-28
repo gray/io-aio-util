52014f6b83c94720875ec34e1b32d6339d7843f3
gray <gray@cpan.org>
2019-02-27 19:14:24 -0600

    Update copyright dates

--- lib/IO/AIO/Util.pm
+++ lib/IO/AIO/Util.pm
@@ -149,7 +149,7 @@ L<http://search.cpan.org/dist/IO-AIO-Util/>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2007-2009 gray <gray at cpan.org>, all rights reserved.
+Copyright (C) 2007-2019 gray <gray at cpan.org>, all rights reserved.
 
 This library is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
